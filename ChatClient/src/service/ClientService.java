d1eef9778edc3e17ba04bc452153ee858a505341
TheHiDId <eope1318@gmail.com>
2025-02-24 19:21:43 +0900

    1%

--- ChatClient/src/service/ClientService.java
+++ ChatClient/src/service/ClientService.java
@@ -1,5 +1,11 @@
 package service;
 
-public class ClientService {
+import dto.Client;
 
+public class ClientService {
+	public void setNickName(String newNickName) {
+		Client client = new Client();
+		
+		client.setNickName(newNickName);
+	}
 }
--- ChatClient/src/view/ClientView.java
+++ ChatClient/src/view/ClientView.java
@@ -12,18 +12,62 @@ public class ClientView {
 	
 	// 기본 생성자
 	public ClientView() {
-		service = new ClientService();
-		br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			service = new ClientService();
+			
+			br = new BufferedReader(new InputStreamReader(System.in));
+			
+		} catch (Exception e) {
+			System.err.println("!!An Error Occurred, Quit the Program!!");
+			
+			e.printStackTrace();
+			
+			System.exit(0);
+		}
 	}
 	
 	public void startView() {
 		int inputNum = 0;
+		
+		do {
+			try {
+				inputNum = selectMenu();
+				
+				switch (inputNum) {
+				case 1: startChat(); break;
+				case 2: setNickName(); break;
+				case 0: System.out.println("Quit the Program"); 
+								br.close(); 
+				break;
+				
+				default: System.out.println("Input Wrong Number, Try Again!"); break;
+				}
+				
+			} catch (NumberFormatException e) {
+				System.err.println("Input Only Number!");
+				
+				e.printStackTrace();
+				
+			} catch (IOException e) {
+				System.err.println("Input/Output Exception Occurs!");
+				
+				e.printStackTrace();
+				
+			} catch (Exception e) {
+				System.err.println("Unknown Error Occurs!");
+				
+				e.printStackTrace();
+			}
+			
+		} while (inputNum != 0);
 	}
-	
+
 	public int selectMenu() throws NumberFormatException, IOException {
 		System.out.println("[Chatting App]\n");
 		System.out.println("1. Start Chat");
 		System.out.println("2. Set NickName");
+		
+		System.out.println("\n0. Exit Chatting App");
 		
 		System.out.println();
 		
@@ -33,5 +77,18 @@ public class ClientView {
 		System.out.println();
 		
 		return input;
+	}
+	
+	private void startChat() {
+		
+	}
+
+	private void setNickName() throws IOException {
+		System.out.println("[Set NickName]\n");
+		
+		System.out.print("Input NickName that You Use in Chat >> ");
+		String setNickName = br.readLine();
+		
+		System.out.println("NickName Setting Done!");
 	}
 }
